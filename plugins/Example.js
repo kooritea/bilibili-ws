2045f27e4aa1164f328582e5b29e5b7a692e4b23
kooritea <ochatokori@gmail.com>
2020-05-13 15:00:00 +0800

    插件加载提示,初始化完房间信息再加载插件,可以通过插件静态属性disable禁用插件

--- plugins/Example.js
+++ plugins/Example.js
@@ -1,4 +1,5 @@
 module.exports = class Example {
+  static disable = true;
   constructor(ebus) {
     ebus.on("cmd", ({ message, roomInfo }) => {
       // message
@@ -17,17 +18,19 @@ module.exports = class Example {
        *  roomid: '9389401', // 来自config
        *  meta: {},// 来自config
        *  nickname: '時雨羽衣Official'// 自动根据roomid获取
+       *  _roomid: 9389401, // 用户填写的roomid可能是短id,这个必定是长id
+          masterid: 主播uid
        * }
        */
-      switch (message.cmd) {
-        case "WELCOME":
-          console.log(
-            `欢迎${message.data.uname}进入${roomInfo.nickanme}的房间`
-          );
-          break;
-        default:
-          console.log(message);
-      }
+      // switch (message.cmd) {
+      //   case "WELCOME":
+      //     console.log(
+      //       `欢迎${message.data.uname}进入${roomInfo.nickanme}的房间`
+      //     );
+      //     break;
+      //   default:
+      //     console.log(message);
+      // }
     });
   }
 };
